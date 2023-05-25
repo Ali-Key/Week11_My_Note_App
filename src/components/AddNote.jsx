34d718f91c4ffb80a16d158f90399a25d57e4ae0
Ali-Key <mafiobio700@gmail.com>
2023-05-25 21:13:37 +0300

    My frist commit

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,14 +1,67 @@
 import React from 'react';
-
 // Import Formik and Yup
+import { Formik, Form , Field , ErrorMessage} from 'formik';
+import * as Yup from 'yup';
 
-const AddNote = () => {
- 
+const AddNote = (props) => {
+
   // add form logic here
 
+  const {AddNote} = props;
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required('Add title note'),
+    content: Yup.string().required('Add description note')
+  });
+
+  const initialValues = {
+    title: '',
+    content: ''
+  };
+
+  const handleSubmit = (values) => {
+    console.log(values)
+    AddNote({
+      title: values.title,
+      content: values.content
+
+    });
+
+    values.title = '';
+    values.content = '';
+
+  };
+
   return (
-    <div>
-      { /* Add here your form and style it with Tailwind */ }
+    <div className="p-6 w-[61%] bg-white rounded" id="form-box">
+     <Formik
+       initialValues={initialValues}
+       validationSchema={validationSchema}
+       onSubmit={handleSubmit}
+     >
+      <Form className="p-3 flex flex-col gap-5 ">
+
+        <Field 
+          type="text"
+          name="title"
+          placeholder="Enter the title......"
+          className="w-full p-3 border-2 rounded outline-none"
+          />
+
+        <ErrorMessage name="title" component="div" className="border-l-4 border-[#cf3d3d] p-4 text-sm text-[#cf3d3d] shadow rounded"/>
+         
+         <Field 
+          as="textarea"
+          name="content"
+          placeholder="Enter the description........."
+          className="w-full p-3 border-2 rounded outline-none"
+         />
+
+        <ErrorMessage name="content" component="div" className="border-l-4 border-[#cf3d3d] p-4 text-sm text-[#cf3d3d] shadow rounded"/>
+         <button type="submit" className="p-3 bg-yellow-300 rounded-lg text-white hover:bg-yellow-500">Add Note</button>
+      </Form>
+
+     </Formik>
     </div>
   );
 };
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -3,8 +3,23 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 
 function Notes(props) {
   return (
-    <div>
+    <div className=" flex flex-auto items-center justify-center gap-7 mt-8 p-8">
+
       { /* Style your note cards with Tailwind here and get the data from app.js */ }
+      
+      {notes.map(note => (
+      <div key={note.id} className="flex flex-col items-center justify-center gap-9">
+        
+        <h1 className="text-[#fff]  font-medium"> {note.title}</h1>
+        <p className="text-white text-left pb-10 overflow-hidden">{note.content}</p>
+
+         <div className="w-full flex   text-[#fff] justify-between items-center">
+          <FaEdit className="cursor-pointer"/>
+          <FaTrash onClick={() => deleteNote(note.id)} className="cursor-pointer "/>
+         </div>
+         
+      </div>
+      ))}
     </div>
   );
 }
